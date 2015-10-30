dab15d1ba4a299a100a463b697accd90c96622ab
Elise Morrison <elisesmorrison@gmail.com>
2015-10-30 12:28:35 -0400

    Renamed houseelf_earlength_dna_data.csv to ...data_1.csv.  Change the data import script to import data_1 file

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -2,16 +2,25 @@ library(vegan)
 library(ggplot2)
 library(stringr)
 
-#1
+#1: done
+
+#2
 #this takes data on house elf dna and ear length and assigns them to groups
 #based on ear length, and calculates the gc content for house elf dna
 
-#2
+#3
 #import data into R
 he_data<-read.csv("data/houseelf_earlength_dna_data.csv", head=TRUE, sep=",")
 head(he_data)
 
-#3
+#4: change data to import data_1 file
+he_data<-read.csv("data/houseelf_earlength_dna_data_1.csv", head=TRUE, sep=",")
+head(he_data)
+
+
+#5: done
+
+#6: function to calculate GC content, regardless of sequence case
 g_c_content<-function(dnaseq){
   upper<-str_to_upper(dnaseq)
   Gs<-str_count(upper, 'G')
@@ -20,3 +29,5 @@ g_c_content<-function(dnaseq){
   return(gc_content)
 }
 g_c_content(he_data$dnaseq)
+
+
