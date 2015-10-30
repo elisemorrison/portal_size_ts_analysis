133ff6f69a92904350e5812a10baf1e72ddb1fa6
Elise Morrison <elisesmorrison@gmail.com>
2015-10-30 12:13:27 -0400

    Adding funciton to calculate GC content

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -1,7 +1,22 @@
+library(vegan)
+library(ggplot2)
+library(stringr)
+
+#1
 #this takes data on house elf dna and ear length and assigns them to groups
 #based on ear length, and calculates the gc content for house elf dna
 
-get_data<-function(){
-  data<-read.csv("surveys.csv")
-  return(data)
+#2
+#import data into R
+he_data<-read.csv("data/houseelf_earlength_dna_data.csv", head=TRUE, sep=",")
+head(he_data)
+
+#3
+g_c_content<-function(dnaseq){
+  upper<-str_to_upper(dnaseq)
+  Gs<-str_count(upper, 'G')
+  Cs<-str_count(upper, 'C')
+  gc_content<-(Gs + Cs)/str_length(upper)*100
+  return(gc_content)
 }
+g_c_content(he_data$dnaseq)
