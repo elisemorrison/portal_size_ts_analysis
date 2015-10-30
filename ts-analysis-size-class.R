01f2d1770ce4c2e3f262ebd2778f5c55ec30737f
Elise Morrison <elisesmorrison@gmail.com>
2015-10-30 12:28:56 -0400

    Edited size class function

--- ts-analysis-size-class.R
+++ ts-analysis-size-class.R
@@ -4,3 +4,22 @@ get_data<-function(){
 }
 
 get_data()
+
+get_size_class<-function(weight, threshold){
+  if(weight>threshold){
+    size_class<-"large"
+  }
+  else{
+    size_class<-"small"
+  }
+  return(size_class)
+}
+
+add_size_class<-function(df){
+  data_w_size_class<-
+    df %>% 
+    na.omit() %>%
+    rowise() %>%
+    mutate(size_class=get_size_class(weight, 50))
+  return(data_w_size_class)
+}
