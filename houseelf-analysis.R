a5d3985a0a95d0f68289bd36538d0288165601e3
Elise Morrison <elisesmorrison@gmail.com>
2015-10-30 15:42:49 -0400

    Make data frame with GC content and ear length class for each individual

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -1,6 +1,7 @@
 library(vegan)
 library(ggplot2)
 library(stringr)
+library(dplyr)
 
 #1: done
 
@@ -29,12 +30,21 @@ g_c_content<-function(dnaseq){
   return(gc_content)
 }
 
+#7
 get_size_class <- function(seq){
    #Calculate the GC-content for one or more sequences
    ear_lengths <- ifelse(seq > 10, "large", "small")
    return(ear_lengths)
 }
 
-g_c_content(he_data$dnaseq)
+make_df_gc_earlength<-function(df){
+  df %>%
+  mutate(gc = g_c_content(dnaseq)) %>%
+  mutate(ear_length=get_size_class(earlength)) %>%
+  subset(select=-c(earlength, dnaseq)) %>%
+  write.csv(file="grangers_analysis.csv", quote=FALSE)
+}
+
+make_df_gc_earlength(he_data)
 
 
